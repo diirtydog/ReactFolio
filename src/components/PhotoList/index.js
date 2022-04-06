6ff9901a735f3f12b580ff12d74e326c069aaca3
diirtydog <andrew.derek.wales@gmail.com>
2022-04-05 19:06:50 -0500

    fixed errors

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -8,19 +8,22 @@ function PhotoList({ category }) {
           name: 'SeeVee',
           category: 'portfolio',
           description: 'This is a social media site intended to help new developers find gainful employment. It utilizes Javascript, HTML, CSS, Nodejs, Sql, sequelize, and handlebars!',
-          link: 'https://see-vee-bnh.herokuapp.com/'
+          link: 'https://see-vee-bnh.herokuapp.com/',
+          key: 'seee'
         },
         {
           name: 'HTML, CSS portfolio',
           category: 'portfolio',
           description: 'This is a bare bones basic website intended to showcase my competencies of HTML, and CSS!',
-          link: 'https://diirtydog.github.io/Portfolio/'
+          link: 'https://diirtydog.github.io/Portfolio/',
+          key: 'port'
         },
         {
           name: 'Trekkie for Techy',
           category: 'portfolio',
           description: 'This is a blogging site that is intended to showcase my competencies in Sql, sequelize, Nodejs, Javascript, HTML, CSS, and handlebars!',
-          link: 'https://awtrekkie.herokuapp.com/'
+          link: 'https://awtrekkie.herokuapp.com/',
+          key: 'trekkie'
         },
     ]);
     const currentPhotos = photos.filter((photo) => photo.category === category);
@@ -30,6 +33,7 @@ function PhotoList({ category }) {
       setCurrentPhoto({...image, index: i});
       setIsModalOpen(!isModalOpen);
     }
+    
     return (
     <div>
       {isModalOpen && (
@@ -37,18 +41,20 @@ function PhotoList({ category }) {
       )}
         <div className="flex-row card">
           {currentPhotos.map((image, i) => (
-              <div>
-                  <div>
+            
+              // <div>
+                  //<div>
                     <img
                         src={require(`../../assets/large/${category}/${i}.jpg`)}
                         alt={image.name}
                         className="img-thumbnail mx-1"
                         onClick={() => toggleModal(image, i)}
-                        key={image.name}
+                        key={image.key}
+                        value={image}
                     />
-                  </div>
-                <a classname='img-a' href={image.link}>Wanna see it 👀!</a>
-              </div>
+                  //</div>
+                //<a className='img-a' href={image.link}>Wanna see it 👀!</a>
+             // </div> */}
           ))}
         </div>
     </div>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,23 +3,23 @@ import React from 'react';
 function Resume() {
     return (
         <section>
-        <h2 class="name">  Andrew Wales </h2>
-            <h3 class="contact job-title">Buna TX | Phone +1 (409) 594 1736 | <a href="mailto: andrew.derek.wales@gmail.com">Email</a> | <a href="https://www.linkedin.com/in/andrew-wales-9b10aa224/">Linked In</a> | <a href="https://github.com/diirtydog">GitHub</a></h3> 
-        <h2 class="section-title">SUMMARY</h2>
-            <p class="skills">Full stack developer ready to take on the world one coding problem at a time. I’m slightly newer to the game but feel as if I’ve started with an excellent foundation by attending the University of Texas at Austin’s coding bootcamp. I’ve attained a vast knowledge base of what exactly a full stack developer should need. It should be noted that I’m currently advancing my education and knowledge base by getting my computer science degree from SNHU. I’ve fallen in love with coding thanks to the program at UT and can’t wait to start an exciting career in tech.</p>
-        <h2 class="section-title">EDUCATION</h2> 
-            <p class="skills">Bachelor of Science in Computer Science currently enrolled:
+        <h2 className="name">  Andrew Wales </h2>
+            <h3 className="contact job-title">Buna TX | Phone +1 (409) 594 1736 | <a href="mailto: andrew.derek.wales@gmail.com">Email</a> | <a href="https://www.linkedin.com/in/andrew-wales-9b10aa224/">Linked In</a> | <a href="https://github.com/diirtydog">GitHub</a></h3> 
+        <h2 className="section-title">SUMMARY</h2>
+            <p className="skills">Full stack developer ready to take on the world one coding problem at a time. I’m slightly newer to the game but feel as if I’ve started with an excellent foundation by attending the University of Texas at Austin’s coding bootcamp. I’ve attained a vast knowledge base of what exactly a full stack developer should need. It should be noted that I’m currently advancing my education and knowledge base by getting my computer science degree from SNHU. I’ve fallen in love with coding thanks to the program at UT and can’t wait to start an exciting career in tech.</p>
+        <h2 className="section-title">EDUCATION</h2> 
+            <p className="skills">Bachelor of Science in Computer Science currently enrolled:
             Southern New Hampshire University - NH </p>
-        <h2 class="section-title">SKILLS</h2>       
-           <div class="skills"> 
+        <h2 className="section-title">SKILLS</h2>       
+           <div className="skills"> 
                 <p>•     Hyper Text Markup Language, Cascading Style Sheets, Javascript</p>
                 <p>•     Knowledge of MySQL, MySQL2, node, express, handlebars, React, inquirer, sequelize, and many more!</p> 
                 <p>•     Logical troubleshooting skills, ability to quickly determine the source of a problem and implement a fix</p>
                 <p>•     Have a vast knowledge of what the consumer needs as well. I’ve lived the life of someone who does not know everything about computers and believe I have an edge on the competition when it comes to making a program very user friendly.</p>
             </div>
-        <h2 class="section-title">WORK EXPERIENCE</h2> 
-            <h3 class="job-title">Personal Web Development, 10/2021 – present</h3>
-                <div class="skills">
+        <h2 className="section-title">WORK EXPERIENCE</h2> 
+            <h3 className="job-title">Personal Web Development, 10/2021 – present</h3>
+                <div className="skills">
                     <p>•	Project Prestige Worldwide was a full stack application utilizing handlebars, MySQL, and Javascript just to name a few. It is a developer job finding application my cohorts and I thought would be useful in a real world application.</p>
                     <p><a href="https://github.com/GunnySensei/project_prestige_worldwide">Project Prestige Worldwide</a></p>
                     <p>•	Trekkie For Techy is a full stack application utilizing handlebars, MySQL, CSS, and javascript.  It is a blog focused application that is meant to showcase my ability to implement sequelize and handlebars.</p>
@@ -35,24 +35,24 @@ function Resume() {
                     <p>•	Password Generator is a front-end application that utilizes CSS, HTML, and Javascript to generate a safe and secure password within a given criteria.</p>
                     <p><a href="https://github.com/diirtydog/password-generator">Password Generator</a></p>
                 </div>
-            <h3 class="job-title">Crane Operator, 01/2021 – present Allco Construction, Beaumont, TX</h3>
-                <div class="skills">
+            <h3 className="job-title">Crane Operator, 01/2021 – present Allco Construction, Beaumont, TX</h3>
+                <div className="skills">
                     <p>•	Performs daily inspection on cranes and any other peripheral equipment on the site.</p> 
                     <p>•	Observes the best and safest work practices available today in the construction industry.</p>
                     <p>•	Ensure that every load lifted is rigged in a proper and professional fashion, that is in line with OSHA standards and regulations.</p>
                     <p>•	Perform the duties of the superintendent whenever he is otherwise unavailable</p>
                     <p>•	Develops new lift plans that are associated with the particular lift we are performing on the job.</p>
                 </div>
-            <h3 class="job-title">Crane Operator, 08/2018– 01/2021 C.A. Murren, Atlanta, GA</h3> 
-                <div class="skills">
+            <h3 className="job-title">Crane Operator, 08/2018– 01/2021 C.A. Murren, Atlanta, GA</h3> 
+                <div className="skills">
                     <p>•	Performs daily inspection on cranes and any other peripheral equipment on the site.</p> 
                     <p>•	Observes the best and safest work practices available today in the construction industry.</p>
                     <p>•	Ensure that every load lifted is rigged in a proper and professional fashion, that is in line with OSHA standards and regulations.</p>
                     <p>•	Perform the duties of the superintendent whenever he is otherwise unavailable</p>
                     <p>•	Develops new lift plans that are associated with the particular lift we are performing on the job.</p>
                 </div>
-            <h3 class="job-title">Piledriving Foreman, 03/2011 – 08/2018 Bomac Contractors, Beaumont, TX</h3> 
-                <div class="skills">
+            <h3 className="job-title">Piledriving Foreman, 03/2011 – 08/2018 Bomac Contractors, Beaumont, TX</h3> 
+                <div className="skills">
                     <p>•	Performs daily inspection on cranes and any other peripheral equipment on the site.</p> 
                     <p>•	Observes the best and safest work practices available today in the construction industry.</p>
                     <p>•	Ensure that every load lifted is rigged in a proper and professional fashion, that is in line with OSHA standards and regulations.</p>
@@ -60,8 +60,8 @@ function Resume() {
                     <p>•	Advised the crew on how we were going to complete the tasks given that day.</p>
                     <p>•	Alerted the supervisor of any safety concerns and helped keep the peace between the crew members.</p>
                 </div>
-            <h3 class="job-title">Soldier, 11/2006 – 03/2011 United States Army, Fort Carson, CO</h3>
-                <div class="skills">
+            <h3 className="job-title">Soldier, 11/2006 – 03/2011 United States Army, Fort Carson, CO</h3>
+                <div className="skills">
                     <p>•	Performs daily inspection on trucks and any other peripheral equipment on the site.</p> 
                     <p>•	Completed two combat tours in the great nation of Iraq.</p>
                     <p>•	Was awarded numerous awards and commendations and will provide a DD214 upon request.</p>
